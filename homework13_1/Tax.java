a184d4e3209a5db38f2108c90a93c986fe250510
tatianaburii <azazazazazaz3333@gmail.com>
2021-12-17 18:37:17 +0200

    add change in method and class

--- homework13_1/Tax.java
+++ homework13_1/Tax.java
@@ -20,34 +20,46 @@ public class Tax {
                 System.out.println(e.getMessage());
             }
         }
-
         for (RealEstate taxes1 : realEstates) {
             System.out.println("Tax = " + taxes1.calculateTax());
         }
     }
 
-    public static RealEstate createRealEstate(RealEstateType type) throws IOException{
-        RealEstate realEstate = null;
-            switch (type) {
-                case DWELLING:
-                    System.out.println("Input: S, taxK, percent.");
-                    realEstate = new Dwelling(inputData(),inputData(),inputData());
-                    break;
-                case RETAIL:
-                    System.out.println("Input: S, taxK, revenue.");
-                    realEstate = new Retail(inputData(),inputData(),inputData());
-                    break;
-
-                case PRODUCTION:
-                    System.out.println("Input: S, taxK.");
-                    realEstate = new Production(inputData(),inputData());
-                    break;
-            }
-        return realEstate;
+    public static RealEstate createRealEstate(RealEstateType type) throws IOException {
+        switch (type) {
+            case DWELLING:
+                System.out.println("Input: S, taxK, percent.");
+                return new Dwelling(inputData(), inputData(), inputPercent());
+            case RETAIL:
+                System.out.println("Input: S, taxK, revenue.");
+                return new Retail(inputData(), inputData(), inputData());
+            case PRODUCTION:
+                System.out.println("Input: S, taxK.");
+                return new Production(inputData(), inputData());
+            default:
+                throw new IllegalStateException("Unexpected value: " + type);
+        }
     }
+
     public static int inputData() throws IOException {
         int input;
         input = Integer.parseInt(reader.readLine());
+
+        if (input < 0) {
+            System.out.println("Data entered incorrectly, please, try again");
+            inputData();
+        }
+        return input;
+    }
+
+    public static int inputPercent() throws IOException {
+        int input;
+        input = Integer.parseInt(reader.readLine());
+
+        if (input < 0 || input > 100) {
+            System.out.println("Data entered incorrectly, please, try again");
+            inputPercent();
+        }
         return input;
     }
 }
