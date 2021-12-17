74a688ac659445a8bc7594d1715eab9c0eeabeb0
tatianaburii <azazazazazaz3333@gmail.com>
2021-12-17 19:24:39 +0200

    add change in method and class

--- elementary_homework_2/Bank.java
+++ elementary_homework_2/Bank.java
@@ -14,17 +14,29 @@ public class Bank {
         double percent = Double.parseDouble(reader.readLine());
         System.out.println("Input years:");
         byte years = Byte.parseByte(reader.readLine());
+
         Bank bank = new Bank();
-        System.out.println("All sum: " + bank.pow(months, money, percent, years));
-        System.out.println("Percent: " + (bank.pow(months, money, percent, years) - money));
+
+        System.out.println("All sum: " + bank.calculateAllSum(months, money, percent, years));
+        System.out.println("Percent: " + (bank.calculateAllSum(months, money, percent, years) - money));
+        bank.calculateAndPrintSum(months, money, percent, years);
 
     }
 
-    public double pow(byte months, double money, double percent, byte years) {
+    public double calculateAllSum(byte months, double money, double percent, byte years) {
         double a = 1 + percent / (100 * months);
         double b = months * years;
         return money * Math.pow(a, b);
 
+    }
+
+    public void calculateAndPrintSum(byte months, double money, double percent, byte years) {
+        for (int i = 1; i < years; i++) {
+            double a = 1 + percent / (100 * months);
+            double b = months * i;
+            double sum = money * Math.pow(a, b);
+            System.out.println("Sum after " + i + " year: " + sum);
+        }
     }
 
 
